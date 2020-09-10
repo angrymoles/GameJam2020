66ee832b88a939233d68b84ae2fea8bd33520e50
angrymoles <reallyangrymoles@gmail.com>
2020-09-10 13:18:21 -0500

    Shadow and Shield are both now mostly functional

--- AngryMoles_GJ2020/Assets/Scripts/PlayerAction.cs
+++ AngryMoles_GJ2020/Assets/Scripts/PlayerAction.cs
@@ -9,28 +9,94 @@ public class PlayerAction : MonoBehaviour
     {
         E_NORMAL,
         E_SHADOW,
+        E_SHIELD,
         E_DEAD,
     }
 
     public Lamp lamp;
-    private PLAYER_STATE playerState;
+    public PolygonCollider2D shieldCollider;
+    public PolygonCollider2D bodyCollider;
+    //private PLAYER_STATE playerState;
+    private bool shadowActive = false;
+    private bool shieldActive = false;    
     // Start is called before the first frame update
     void Start()
     {
-        playerState = PLAYER_STATE.E_NORMAL;
+        //playerState = PLAYER_STATE.E_NORMAL;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        bool leftDown = Input.GetMouseButton(0);
+        bool rightDown = Input.GetMouseButton(1);
+
+        if (leftDown && rightDown )
         {
-            lamp.SetPower(-Time.deltaTime);
+            ActivateBomb();
+        }
+        else if (leftDown)
+        {
+            ToggleShield(true);
+            ToggleShadow(false);
+        }
+        else if (rightDown)
+        {
+            ToggleShadow(true);
+            ToggleShield(false);
+            //lamp.SetPower(-Time.deltaTime);
         }
         else
         {
-            lamp.SetPower(Time.deltaTime);
+            ToggleShield(false);
+            ToggleShadow(false);
         }
+        //else
+        //{
+        //    lamp.SetPower(Time.deltaTime);
+        //}
+    }
+
+    private void ToggleShadow(bool active)
+    {
+        if (shadowActive != active)
+        {
+            shadowActive = active;
+            if (shadowActive)
+            {
+                lamp.ActivateShadow();
+                bodyCollider.enabled = false;
+                shieldCollider.enabled = false;
+            }
+            else
+            {
+                lamp.DeactivateShadow();
+                bodyCollider.enabled = true;
+            }
+        }
+    }
+
+    private void ToggleShield(bool active)
+    {
+        if ( shieldActive != active)
+        {
+            shieldActive = active;
+            if (shieldActive)
+            {
+                lamp.ActivateShield();
+                shieldCollider.enabled = true;
+            }
+            else
+            {
+                lamp.DeactivateShield();
+                shieldCollider.enabled = false;
+            }
+        }        
+    }
+
+    private void ActivateBomb()
+    {
+        Debug.Log("Bomb activated");
     }
 
     void SetShadowMode(bool bShadow)
