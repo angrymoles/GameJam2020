e051f2ee0226ec376c397e497d381427aff415d8
Yong-Ik Jo <yong-ik.jo@aeriagames.com>
2020-09-10 18:35:46 +0200

    enemy shot
    add function : enemy shot bullet to player

--- AngryMoles_GJ2020/Assets/Scripts/Shooting.cs
+++ AngryMoles_GJ2020/Assets/Scripts/Shooting.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    public Transform PlayerTransform;
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 20f;
@@ -32,6 +33,9 @@ public class Shooting : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        Vector3 dirction = PlayerTransform.position - firePoint.position;
+        dirction.Normalize();
+
+        rb.AddForce(dirction * bulletForce, ForceMode2D.Impulse);
     }
 }
