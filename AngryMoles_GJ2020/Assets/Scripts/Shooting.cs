13f94aabea7b1a6688ae14176cc38359cb400354
Rudolf Teise <Teise-rudolf@hotmail.de>
2020-09-11 05:37:48 +0200

    Bullets, Hits, EnemyDraft

--- AngryMoles_GJ2020/Assets/Scripts/Shooting.cs
+++ AngryMoles_GJ2020/Assets/Scripts/Shooting.cs
@@ -6,7 +6,13 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public float bulletForce = 20f;
+ 
+
+    public float fireRate;
+
+    public Transform target;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +22,21 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.LookAt(target);
     }
 
     IEnumerator ShootRoutine()
     {
-        while (true)
-        {
+       
             Shoot();
-            yield return new WaitForSeconds(2);
-        }
+            yield return new WaitForSeconds(fireRate);
+        
     }
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position,firePoint.rotation);
+               
     }
+
 }
--- AngryMoles_GJ2020/Assets/Scripts/bullet.cs
+++ AngryMoles_GJ2020/Assets/Scripts/bullet.cs
@@ -1,37 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
-    public float destroyHitEffectTime = 1f;
-    public float lifeTime = 10f;
+    public GameObject[] hitEffect;
+    public float destroyHitEffectTime = 2f;
+    public float lifeTime = 300f;
     public float damage = 1f;
+    public float bulletSpeed = 5f;
+    private Rigidbody2D rb;
+    public GameObject myLight;
+   
     //public UnityEngine.Experimental.Rendering.Universal.Light2D attachedLight;
 
     private float timeSpan = 0f;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (hitEffect != null)
+        // 9 is the Player Layer
+        if (collision.gameObject.layer==9)
         {
-            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            GameObject effect = Instantiate(hitEffect[0], transform.position, Quaternion.identity);
+            Destroy(effect, destroyHitEffectTime);
+            Destroy(gameObject);
+        }
+        //10 is the wall layer
+        if (collision.gameObject.layer==10)
+        {
+            GameObject effect = Instantiate(hitEffect[1], transform.position, Quaternion.identity);
             Destroy(effect, destroyHitEffectTime);
         }
 
-        Destroy(gameObject);
+        if (collision.gameObject.layer == 11)
+        {
+            GameObject effect = Instantiate(hitEffect[2], transform.position, Quaternion.identity);
+            Destroy(effect, destroyHitEffectTime);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.layer == 12|| collision.gameObject.layer == 13)
+        {
+            GameObject effect = Instantiate(hitEffect[3], transform.position, Quaternion.identity);
+            Destroy(effect, destroyHitEffectTime);
+            
+        }
+
+
+
+
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        timeSpan = 0.0f;
+     
+        bulletSpeed = Random.Range(3, 12);
+        myLight.GetComponent<Light2D>().color = new Color(Random.Range(0F, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
+        //timeSpan = 0.0f;
+
+        Debug.Log(bulletSpeed);
+        GetComponent<Rigidbody2D>().AddForce(transform.forward * bulletSpeed);
+
+        GetComponent<SpriteRenderer>().color=Random.ColorHSV();
+        
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.up;
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        LifeTimeProcess();
+        
+        //LifeTimeProcess();
     }
 
     void LifeTimeProcess()
