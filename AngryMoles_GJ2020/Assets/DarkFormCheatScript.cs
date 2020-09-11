99d06f546dc47d2ddad7f6074dea2dc001bafa7b
Rudolf Teise <Teise-rudolf@hotmail.de>
2020-09-11 12:41:38 +0200

    ShadowFormImplementation

---
+++ AngryMoles_GJ2020/Assets/DarkFormCheatScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DarkFormCheatScript : MonoBehaviour
+{
+    public GameObject darkform;
+    public GameObject Lamp;
+    public GameObject PlayerAnimation;
+
+    private void Start()
+    {
+        darkform.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)){
+            darkform.SetActive(true);
+            Lamp.SetActive(false);
+            PlayerAnimation.GetComponent<SpriteRenderer>().enabled = false;
+           
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            darkform.SetActive(false);
+            Lamp.SetActive(true);
+            PlayerAnimation.GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+
+    }
+}
